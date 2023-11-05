89b0b0167f9e33f8ffaa24ed44051c30f9cd4c44
markpan07 <markpan07@mail.ru>
2023-11-05 12:23:04 +0300

    Initial commit

---
+++ src/main/java/pro/sky/hogwarts3_6/Hogwarts36Application.java
@@ -0,0 +1,13 @@
+package pro.sky.hogwarts3_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hogwarts36Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hogwarts36Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/hogwarts3_6/Hogwarts36ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hogwarts3_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hogwarts36ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
