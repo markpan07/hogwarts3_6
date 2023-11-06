8a4f1edbb3adeb568fb9553e72e85ab62b67d3c7
markpan07 <markpan07@mail.ru>
2023-11-06 10:51:08 +0300

    App is working. Student controller and service CRUD have been done

--- src/main/java/pro/sky/hogwarts3_6/controller/StudentController.java
+++ src/main/java/pro/sky/hogwarts3_6/controller/StudentController.java
@@ -1,4 +1,44 @@
 package pro.sky.hogwarts3_6.controller;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import pro.sky.hogwarts3_6.dto.FacultyDtoIn;
+import pro.sky.hogwarts3_6.dto.FacultyDtoOut;
+import pro.sky.hogwarts3_6.dto.StudentDtoIn;
+import pro.sky.hogwarts3_6.dto.StudentDtoOut;
+import pro.sky.hogwarts3_6.repository.StudentRepository;
+import pro.sky.hogwarts3_6.service.FacultyService;
+import pro.sky.hogwarts3_6.service.StudentService;
+
+@RestController
+@RequestMapping("/students")
 public class StudentController {
+
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("{id}")
+    public StudentDtoOut getStudent(@PathVariable Long id) {
+        return studentService.getStudent(id);
+    }
+
+    @PostMapping()
+    public StudentDtoOut postStudent(@RequestBody StudentDtoIn dto) {
+        return studentService.createStudent(dto);
+    }
+
+    @PutMapping("{id}")
+    public StudentDtoOut updateStudent(@PathVariable Long id,
+                                       @RequestBody StudentDtoIn dto) {
+        return studentService.updateStudent(id, dto);
+    }
+
+    @DeleteMapping("{id}")
+    public StudentDtoOut deleteStudent(@PathVariable Long id) {
+        return studentService.deleteStudent(id);
+    }
 }
--- src/main/java/pro/sky/hogwarts3_6/service/StudentService.java
+++ src/main/java/pro/sky/hogwarts3_6/service/StudentService.java
@@ -1,4 +1,57 @@
 package pro.sky.hogwarts3_6.service;
 
+import org.springframework.stereotype.Service;
+import pro.sky.hogwarts3_6.dto.StudentDtoIn;
+import pro.sky.hogwarts3_6.dto.StudentDtoOut;
+import pro.sky.hogwarts3_6.exception.FacultyNotFoundException;
+import pro.sky.hogwarts3_6.exception.StudentNotFoundException;
+import pro.sky.hogwarts3_6.mapper.FacultyMapper;
+import pro.sky.hogwarts3_6.mapper.StudentMapper;
+import pro.sky.hogwarts3_6.model.Student;
+import pro.sky.hogwarts3_6.repository.AvatarRepository;
+import pro.sky.hogwarts3_6.repository.FacultyRepository;
+import pro.sky.hogwarts3_6.repository.StudentRepository;
+
+@Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+    private final FacultyRepository facultyRepository;
+    private final StudentMapper studentMapper;
+    private final FacultyMapper facultyMapper;
+    private final AvatarRepository avatarRepository;
+
+    public StudentService(StudentRepository studentRepository,
+                          FacultyRepository facultyRepository,
+                          StudentMapper studentMapper,
+                          FacultyMapper facultyMapper,
+                          AvatarRepository avatarRepository) {
+        this.studentRepository = studentRepository;
+        this.facultyRepository = facultyRepository;
+        this.studentMapper = studentMapper;
+        this.facultyMapper = facultyMapper;
+        this.avatarRepository = avatarRepository;
+    }
+
+    public StudentDtoOut getStudent(Long id) {
+        return studentMapper.toDto(studentRepository.findById(id).orElseThrow(()-> new StudentNotFoundException(id)));
+    }
+
+    public StudentDtoOut createStudent(StudentDtoIn dto) {
+        return studentMapper.toDto(studentRepository.save(studentMapper.toEntity(dto)));
+    }
+
+    public StudentDtoOut updateStudent(Long id, StudentDtoIn dto) {
+        Student oldStudent = studentRepository.findById(id).orElseThrow(()-> new StudentNotFoundException(id));
+        oldStudent.setAge(dto.getAge());
+        oldStudent.setName(dto.getName());
+        oldStudent.setFaculty(facultyRepository.findById(dto.getFacultyId()).orElseThrow(()-> new FacultyNotFoundException(dto.getFacultyId())));
+        return studentMapper.toDto(studentRepository.save(oldStudent));
+    }
+
+    public StudentDtoOut deleteStudent(Long id) {
+        StudentDtoOut dto = studentMapper.toDto(studentRepository.findById(id).orElseThrow(()-> new StudentNotFoundException(id)));
+        studentRepository.deleteById(id);
+        return dto;
+    }
 }
